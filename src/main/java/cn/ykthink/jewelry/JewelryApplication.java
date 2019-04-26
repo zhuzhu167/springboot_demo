04c06c7a77599f5a21f379008118670cfaad3f9c
FYKANG <f963706245@126.com>
2019-04-26 10:26:16 +0800

    'git_init'

---
+++ src/main/java/cn/ykthink/jewelry/JewelryApplication.java
@@ -0,0 +1,13 @@
+package cn.ykthink.jewelry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JewelryApplication.class, args);
+    }
+
+}
