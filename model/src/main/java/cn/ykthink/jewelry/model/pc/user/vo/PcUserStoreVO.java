caa58a27d80fee96db73c2205b15a09454ad2a17
qweasdzxc10177 <453246846@qq.com>
2019-05-15 22:27:09 +0800

    商品查询（未完成）

---
+++ model/src/main/java/cn/ykthink/jewelry/model/pc/user/vo/PcUserStoreVO.java
@@ -0,0 +1,22 @@
+package cn.ykthink.jewelry.model.pc.user.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * program: jewelry
+ * description:库存VO
+ * author: mi
+ * create: 2019-05-15 20:40
+ **/
+@Data
+public class PcUserStoreVO {
+    @ApiModelProperty(value = "库存uuid", name = "storeUuid", example = "1234", dataType = "String", required = true)
+    private String storeUuid;
+
+    @ApiModelProperty(value = "库存总量", name = "storeTotal", example = "1000", dataType = "Integer", required = true)
+    private String storeTotal;
+
+    @ApiModelProperty(value = "库存消耗量", name = "storeConsumption", example = "10", dataType = "Integer", required = true)
+    private String storeConsumption;
+}
